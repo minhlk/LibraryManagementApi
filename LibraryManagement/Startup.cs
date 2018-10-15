027ad56305e95cd43f3c1966c874c129f80104cb
Duy Minh <minh.bd96@outlook.com.vn>
2018-10-15 19:34:42 +0700

    Add User Model

--- LibraryManagement/Startup.cs
+++ LibraryManagement/Startup.cs
@@ -1,33 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LibraryManagement.Config;
 using LibraryManagement.Data;
 using LibraryManagement.Data.Interface;
-using LibraryManagement.Models;
 using LibraryManagement.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Swashbuckle.AspNetCore.Swagger;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace LibraryManagement
 {
     public class Startup
     {
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -41,7 +35,7 @@ namespace LibraryManagement
             // Register the Swagger generator, defining one or more Swagger documents  
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                c.SwaggerDoc("v1", new Info { Title = "MK API", Version = "v1" });
             });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddMvc().AddJsonOptions(options => {
@@ -49,9 +43,24 @@ namespace LibraryManagement
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
             });
 
+           //TODO : Add New Dependency Injection Here
+            services.AddScoped<IUserService,UserService>();
+            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+            services.AddScoped<IGenreRepository, GenreRepository>();
+
+
+
+            services.AddDbContext<LibraryManagementContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("LibraryDB")));
+            // Add functionality to inject IOptions<T>
+            services.AddOptions();
+            // Add our Config object so it can be injected
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+
             // configure jwt authentication
-//            var appSettings = appSettingsSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes("this is my custom Secret key for authnetication");
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("AppSettings").GetSection("Secret").Value);
             services.AddAuthentication(x =>
                 {
                     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -84,13 +93,6 @@ namespace LibraryManagement
                         ValidateAudience = false
                     };
                 });
-            services.AddScoped<IUserService,UserService>();
-            services.AddScoped<IUserRepository,UserRepository>();
-            services.AddScoped<IBookRepository, BookRepository>();
-            services.AddScoped<IGenreRepository, GenreRepository>();
-            services.AddDbContext<LibraryManagementContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("LibraryDB")));
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -117,7 +119,7 @@ namespace LibraryManagement
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.  
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MK API V1");
             });
             app.UseHttpsRedirection();
             app.UseMvc();
