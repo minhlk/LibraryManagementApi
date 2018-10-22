1c0aa34d964b739f2b90cd8e855fb342ff3da3ba
MinhLK <minhmeolavip@gmail.com>
2018-10-22 22:05:51 +0700

    Revert "Diennt"

--- LibraryManagement/Data/BookRepository.cs
+++ LibraryManagement/Data/BookRepository.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagement.Data
 {
-    public class BookRepository : RepositoryBase<Book>, IBookRepository
+    public class BookRepository : RepositoryBase<Book>,IBookRepository
     {
         public BookRepository(LibraryManagementContext repositoryContext) : base(repositoryContext)
         {
@@ -53,9 +53,8 @@ namespace LibraryManagement.Data
         }
 
     }
-    public static class Extension
-    {
-        public static void Map(this Book oldBook, Book newBook)
+    public static class Extension{
+        public static void Map(this Book oldBook,Book newBook)
         {
             //Check this later on
             oldBook.Name = newBook.Name;
@@ -64,5 +63,5 @@ namespace LibraryManagement.Data
             oldBook.BookGenre = newBook.BookGenre;
 
         }
-    }
+}
 }
